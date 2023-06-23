907463006f03107e901ec4340bfa2ff191afc288
agentMAK <martin0044@gmail.com>
2023-06-23 08:46:40 +0100

    Rainbowkit & wagmi setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,31 @@
 "use client"
-
 import { Chakra } from "./chakra";
-import "./globals.css";
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, goerli } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+
+const { chains, publicClient } = configureChains(
+  [mainnet, goerli ,  polygon],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID as string }),
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'Simple dApp',
+  projectId: 'Simple dApp',
+  chains
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 export default function RootLayout({
   children,
@@ -11,7 +35,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Chakra>{children}</Chakra>
+        <Chakra>
+          <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+          </WagmiConfig>
+        </Chakra>
       </body>
     </html>
   );
